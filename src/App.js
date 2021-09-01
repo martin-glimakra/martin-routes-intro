52ba16ba56a8c268263736199213db12446c4496
martin-glimakra <88892064+martin-glimakra@users.noreply.github.com>
2021-09-01 12:10:54 +0200

    useParams

--- src/App.js
+++ src/App.js
@@ -1,58 +1,66 @@
-import React from "react";
+import {useEffect, useState} from "react";
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
+ BrowserRouter as Router,
+ Switch,
+ Route,
+ Link,
+ useParams
 } from "react-router-dom";
-
+ 
 export default function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/about-us">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+ 
+ useEffect(() => {
+ fetch('https://jsonplaceholder.typicode.com/posts')
+ .then((response) => response.json())
+ .then((json) => setstate(json));
+ }, [])
+ 
+ const [state, setstate] = useState(null)
+ 
+ return (
+ <Router>
+ <div>
+ <nav>
+ <ul>
+ <li>
+ <Link to="/">Home</Link>
+ </li>
+ <li>
+ <Link to="/about">About</Link>
+ </li>
+ <li>
+ <Link to="/users">Users</Link>
+ </li>
+ </ul>
+ </nav>
+ 
+ {/* A <Switch> looks through its children <Route>s and
+ renders the first one that matches the current URL. */}
+ <Switch>
+ <Route path="/about/:martinsvariabel">
+ <About state={state} />
+ </Route>
+ <Route path="/users">
+ <Users />
+ </Route>
+ <Route path="/">
+ <Home />
+ </Route>
+ </Switch>
+ </div>
+ </Router>
+ );
 }
-
+ 
 function Home() {
-  return <h2>Home</h2>;
+ return <h2>Home</h2>;
 }
-
-function About() {
-  return <h2>About</h2>;
+ 
+function About({state}) {
+ const {martinsvariabel} = useParams()
+ return <h2>id: {martinsvariabel} Title: {state ? state.filter(obj => obj.id == martinsvariabel)[0].title: <div>laddar data</div>}</h2>;
 }
-
+ 
 function Users() {
-  return <h2>Users</h2>;
+ return <h2>Users</h2>;
 }
